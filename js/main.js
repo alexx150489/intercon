5a5448bc5fd558ae88efabc4de24e1cb66483977
alexx150489 <nikolskiy150489@gmail.com>
2017-06-16 16:26:23 +0400

    pravki

--- js/main.js
+++ js/main.js
@@ -1,5 +1,59 @@
 $(document).ready(function() {
 
+	$(".block-header .link-about-us").on("click", function(e) {
+		e.preventDefault();
+		var currentBlock = $(this).attr("href"),
+		currentBlockOffset = $(currentBlock).offset().top;
+		$("html, body").animate({
+			scrollTop: currentBlockOffset
+		}, 500);
+	});
+
+	$(".block-header .link-profit").on("click", function(e) {
+		e.preventDefault();
+		var currentBlock = $(this).attr("href"),
+		currentBlockOffset = $(currentBlock).offset().top;
+		$("html, body").animate({
+			scrollTop: currentBlockOffset
+		}, 500);
+	});
+
+	$(".block-header .link-about-course").on("click", function(e) {
+		e.preventDefault();
+		var currentBlock = $(this).attr("href"),
+		currentBlockOffset = $(currentBlock).offset().top;
+		$("html, body").animate({
+			scrollTop: currentBlockOffset
+		}, 500);
+	});
+
+	$(".block-header .link-certificate").on("click", function(e) {
+		e.preventDefault();
+		var currentBlock = $(this).attr("href"),
+		currentBlockOffset = $(currentBlock).offset().top;
+		$("html, body").animate({
+			scrollTop: currentBlockOffset
+		}, 500);
+	});
+
+	$(".block-header .link-price").on("click", function(e) {
+		e.preventDefault();
+		var currentBlock = $(this).attr("href"),
+		currentBlockOffset = $(currentBlock).offset().top;
+		$("html, body").animate({
+			scrollTop: currentBlockOffset
+		}, 500);
+	});
+
+	$(".block-header .link-contacts").on("click", function(e) {
+		e.preventDefault();
+		var currentBlock = $(this).attr("href"),
+		currentBlockOffset = $(currentBlock).offset().top;
+		$("html, body").animate({
+			scrollTop: currentBlockOffset
+		}, 500);
+	});
+
 	$(".block-request .go-top-btn").on("click", function(e) {
 		e.preventDefault();
 		var currentBlock = $(this).attr("href"),
